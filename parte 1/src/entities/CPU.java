5eb5b5cfd33f327c154a1701a49deacc5569cc52
Lucas Diniz <lucasdiniz@MacBook-Air-de-Lucas.local>
2019-07-15 22:32:22 -0300

    removes previous code

--- parte 1/src/entities/CPU.java
+++
@@ -1,8 +0,0 @@
-package entities;
-
-public class CPU extends Entity {
-
-	public CPU(String name, double mean, double avg, int numInPorts, double[] probPorts) {
-		super(name, mean, avg, numInPorts, probPorts);
-	}
-}
--- parte 1/src/entities/Disk.java
+++
@@ -1,7 +0,0 @@
-package entities;
-
-public class Disk extends Entity {
-	public Disk(String name, double mean, double avg, int numInPorts) {
-		super(name, mean, avg, numInPorts, new double[]{1.0});
-	}
-}
