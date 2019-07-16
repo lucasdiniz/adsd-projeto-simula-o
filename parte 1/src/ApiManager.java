727b0d7ca96b41c38a79fda6bf926eaf34591dee
Lucas Diniz <lucasdiniz@MacBook-Air-de-Lucas.local>
2019-07-15 22:42:35 -0300

    adds required stats to api manager entity

--- parte 1/src/ApiManager.java
+++ parte 1/src/ApiManager.java
@@ -2,7 +2,29 @@ package simulator;
 
 import eduni.simjava.*;
 import eduni.simjava.distributions.*;
-
+/**
+ ** Api manager that will handle requests and forward it to one of 3 handlers
+ ** (get/post, put/delete and head)
+ **/
 class ApiManager extends Sim_entity {
+  private Sim_port in, outPost, outDelete, outHead;
+  private Sim_normal_obj delay;
+  private Sim_random_obj prob;
+  Sim_stat stat;
 
+
+  ApiManager(String name, double mean, double variance, long seed) {
+    super(name);
+    this.delay = new Sim_normal_obj("Delay", mean, variance, seed);
+    this.prob = new Sim_random_obj("Probability", seed);
+    
+    // Required stats = Utilization, service_time(tempo de repsosta)
+    // waiting_time e queue_length (tamanho das filas)
+    stat = new Sim_stat();        
+    stat.add_measure(Sim_stat.QUEUE_LENGTH);    
+    stat.add_measure(Sim_stat.SERVICE_TIME);   
+    stat.add_measure(Sim_stat.UTILISATION);
+    stat.add_measure(Sim_stat.WAITING_TIME);    
+    set_stat(stat);    
+  }
 }
